65afd12460d220735c6d15a22f48241f72a35071
Priyanshu Kumar Pandey <priyanshukum198@gmail.com>
2024-11-08 14:06:44 +0530

    updated server.js

--- server.js
+++ server.js
@@ -8,27 +8,21 @@ app.use(bodyParser.json());
 const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 
 app.post('/shopify-webhook', async (req, res) => {
-    const { theme } = req.body;
+    console.log('Received webhook payload:', req.body);
 
-    if (!theme) {
+    if (!req.body.theme) {
         console.error('Received data does not include a theme object');
-        return res.status(400).send('Theme data is missing');
+        return res.status(400).send('Received data does not include a theme object');
     }
 
-    const { id: store_id, name: theme_name, published_at: updated_at } = theme;
-
-    if (!store_id || !theme_name || !updated_at) {
-        console.error('Missing properties in theme:', theme);
-        return res.status(400).send('Missing required theme properties');
-    }
+    const { id: store_id, name: theme_name, role: theme_role } = req.body.theme;
 
     const slackMessage = {
-        text: `Alert: The theme on store ID *${store_id}* has changed to *${theme_name}* as of *${updated_at}*.`,
+        text: `Alert: The theme on store ID *${store_id}* has changed to *${theme_name}* with role *${theme_role}*.`,
     };
 
     try {
         await axios.post(SLACK_WEBHOOK_URL, slackMessage);
-        console.log('Notification sent to Slack');
         res.status(200).send('Notification sent to Slack.');
     } catch (error) {
         console.error('Error sending to Slack:', error);
