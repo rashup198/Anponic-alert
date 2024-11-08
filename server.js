156f11acd62ea88850b97581d767f99f55a76be2
Priyanshu Kumar Pandey <priyanshukum198@gmail.com>
2024-11-08 14:00:00 +0530

    updated server.js

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/T06CXJD4M5K/B0803A87
 
 app.post('/shopify-webhook', async (req, res) => {
     const { theme } = req.body;
+
+    if (!theme) {
+        console.error('Received data does not include a theme object');
+        return res.status(400).send('Theme data is missing');
+    }
+
     const { id: store_id, name: theme_name, published_at: updated_at } = theme;
+
+    if (!store_id || !theme_name || !updated_at) {
+        console.error('Missing properties in theme:', theme);
+        return res.status(400).send('Missing required theme properties');
+    }
 
     const slackMessage = {
         text: `Alert: The theme on store ID *${store_id}* has changed to *${theme_name}* as of *${updated_at}*.`,
@@ -17,6 +28,7 @@ app.post('/shopify-webhook', async (req, res) => {
 
     try {
         await axios.post(SLACK_WEBHOOK_URL, slackMessage);
+        console.log('Notification sent to Slack');
         res.status(200).send('Notification sent to Slack.');
     } catch (error) {
         console.error('Error sending to Slack:', error);
