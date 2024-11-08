747552abef2ba2a7c12effe45644cf9d7810712e
Priyanshu Kumar Pandey <priyanshukum198@gmail.com>
2024-11-08 13:55:45 +0530

    updated server.js

--- server.js
+++ server.js
@@ -5,21 +5,17 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
-// Slack Webhook URL
 const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 
 app.post('/shopify-webhook', async (req, res) => {
-    // Destructure the theme information from the payload
     const { theme } = req.body;
     const { id: store_id, name: theme_name, published_at: updated_at } = theme;
 
-    // Prepare the message for Slack
     const slackMessage = {
         text: `Alert: The theme on store ID *${store_id}* has changed to *${theme_name}* as of *${updated_at}*.`,
     };
 
     try {
-        // Send the message to Slack
         await axios.post(SLACK_WEBHOOK_URL, slackMessage);
         res.status(200).send('Notification sent to Slack.');
     } catch (error) {
